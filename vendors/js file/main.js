ac1efa67b945e55d869d98c11d4e5f893a6f520d
Monir2427 <90026955+Monir2427@users.noreply.github.com>
2021-09-16 07:19:05 -0700

    Add files via upload

---
+++ vendors/js file/main.js
@@ -0,0 +1,9 @@
+function openManu() {
+    document.getElementById("hambargur").style.width ="100%";
+}
+
+function closManu() {
+    document.getElementById("hambargur").style.width ="0%";
+}
+
+var mixer = mixitup('.work-grid');
